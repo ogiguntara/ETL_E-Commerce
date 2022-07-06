f8f058459a7b8c0be54e7fd74865e96790798e71
ogiguntara <ogiguntara@gmail.com>
2022-07-06 12:47:11 +0700

    update input data to postgresql service

--- script_ver_2/data_initiation_preparation/input_data_postgresql.py
+++ script_ver_2/data_initiation_preparation/input_data_postgresql.py
@@ -1,10 +1,27 @@
 #!python3
-
 import pandas as pd
+from connection.postgresql import PostgreSQL
+import json
+from sqlalchemy import text
+import time
+
+def ingest_to_postgres(file):
+
+    with open ('data/credentials.json') as cred:
+            credential = json.load(cred) 
+    postgre_auth = PostgreSQL(credential['postgresql_source'])
+    engine, engine_conn = postgre_auth.connect(conn_type='engine')
+    engine_conn.execute(text('CREATE SCHEMA IF NOT EXISTS final_project;'))
+        
+    data = pd.read_csv(file)
+    data.to_sql(name=file[5:-4], con=engine ,if_exists='replace',index=False,schema='final_project')
+    print(f'Table {file[5:-4]} has been created')       
+
 
 if __name__ == '__main__':
     name_list = ['distribution_centers.csv','employees.csv',]
-    data = pd.read_csv('data/users.csv')
-    print(data.head())
-    print(data.shape)
-    print(data.columns)
+    for data in name_list:
+        start_time = time.time()
+        ingest_to_postgres(f'data/{data}')
+        end_time = time.time()
+        print(f'Runtime : {start_time-end_time} second')

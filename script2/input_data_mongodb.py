2a5162b79cf45ad8a26c17d905ca0f324dc20770
ogiguntara <ogiguntara@gmail.com>
2022-07-12 19:28:15 +0700

    add input data to mongo service

--- script2/input_data_mongodb.py
+++ script2/input_data_mongodb.py
@@ -1,40 +1,44 @@
-import csv
-import json
-import pandas as pd
-import pymongo
-import time
+#!python3
+
+import findspark
+findspark.init()
 from pyspark.sql import SparkSession
+import time
 
-# Function to convert a CSV to JSON
-# Takes the file paths as arguments
 
-def input_to_mongo(csvFilePath):
-    spark=SparkSession.builder.appName('data_processing').master("local[1]").getOrCreate()
-    # #USING PYTHON PANDAS
-    # #read csv transform to  dict
-    # if csvFilePath == 'data/events.csv':
-    #     df = pd.read_csv(csvFilePath)
-    #     data = df.to_dict(orient='records')
-    # else : 
-    #     df = pd.read_csv(csvFilePath)
-    #     data = df.to_dict(orient='records')
-    # myclient = pymongo.MongoClient('mongodb://localhost:27017')
-    # mydb = myclient["final_project"]
-    # mycol = mydb[f"{csvFilePath[5:-4]}"]
-    # mycol.delete_many({})
-    # mycol.insert_many(data)
-    data = spark.read.format('csv').option('header','true')\
-                        .load(f'data/{csvFilePath}')
-    #upload / insert many to mongo atlas
-    data.show()
-    print(f"[Project Scenario Preparation] {csvFilePath[5:-4]} has been opload to mongodb",end=" ")
-    spark.stop()
+def input_to_mongo(name_list):
+    input_uri = "mongodb://localhost:27017/"
+    output_uri = "mongodb://localhost:27017/"
+
+    my_spark =  SparkSession\
+                .builder\
+                .appName("MyApp")\
+                .config("spark.mongodb.input.uri", input_uri)\
+                .config("spark.mongodb.output.uri", output_uri)\
+                .config('spark.jars.packages','org.mongodb.spark:mongo-spark-connector_2.12:2.4.2')\
+                .getOrCreate()
+    for file in name_list :
+        start_time = time.time()
+        data = my_spark.read.format('csv')\
+                .option('header','true')\
+                .load(f'file:///home/ogi/projects/end_to_end_final_project/data/{file}')\
+                .limit(1000)
+        
+        data.write.format('mongo').mode('overwrite')\
+                .option('database','final_project')\
+                .option('collection',file[:-4])\
+                .save()
+                
+        end_time = time.time()
+        #log per file stored
+        print(f"[Project Preparation MongoDB] collection '{file[:-4]}' has been created for {round(end_time-start_time,2)} second") 
+    my_spark.stop()
+
     
 
 if __name__ == '__main__':
-    name_list = ['users.csv','inventory_items.csv','order_items.csv','orders.csv','events.csv']
-    for file_name in name_list:
-        start_time = time.time()
-        input_to_mongo(f'data/{file_name}') 
-        end_time = time.time()
-        print(f'for {start_time - end_time} second')
+    print(f"[Project Preparation PostgreSQL] start") 
+    #file name list
+    name_list = ['users.csv','inventory_items.csv','order_items.csv','orders.csv','events.csv',]
+    input_to_mongo(name_list=name_list)
+    print(f"[Project Preparation PostgreSQL] end") 

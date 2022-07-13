88c352ee28cb237444c7300255e558e8869ef607
ogiguntara <ogiguntara@gmail.com>
2022-07-13 12:04:29 +0700

    add comments and test service

--- script2/input_data_postgresql.py
+++ script2/input_data_postgresql.py
@@ -29,13 +29,13 @@ def input_to_postgres(name_list):
         
         #read csv as pandas dataframe
         data = pd.read_csv(f'data/{file}')
-        
+        datacount=data.shape[0]
         #write to postgresql
         data.to_sql(name=file[:-4], con=engine ,if_exists='replace',index=False,schema=schema)
         
         end_time = time.time()
         #log per file stored
-        print(f"[Project Preparation PostgreSQL] table '{file[:-4]}' has been created for {round(end_time-start_time,2)} second") 
+        print(f"[Project Preparation PostgreSQL] table '{file[:-4]}' ({datacount} rows) has been created for {round(end_time-start_time,2)} second") 
         
     #closing every last connection    
     engine.dispose()    

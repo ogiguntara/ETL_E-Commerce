7f49f0ed396b91dd9d51c154c9bf9a6caba6203e
ogiguntara <ogiguntara@gmail.com>
2022-07-16 11:44:43 +0700

    add postgre to hdfs service

--- script2/ingestion.py
+++ script2/ingestion.py
@@ -1,45 +1,58 @@
 #python3
 
 import json
-from unicodedata import name
-from hdfs import InsecureClient
 import time
-import pandas as pd
-from sqlalchemy import create_engine
-import connection
+import findspark
+findspark.init()
+from pyspark.sql import SparkSession
+from connection.postgresql import PostgreSQL
+from datetime import datetime
+
 
 def postgresql_to_hdfs(tables_list):
-    
     #open credential data
     with open ('data/credentials.json') as cred:
             credential = json.load(cred)
-            
+    postgre_auth = PostgreSQL(credential['postgresql_source'])
+    url, properties = postgre_auth.connect(conn_type='spark')
+    
     #input credential data to create engine using connection file 
-    postgre_auth = connection.PostgreSQL(credential['postgresql_source'])
-    engine, engine_conn = postgre_auth.connect(conn_type='engine')
+    my_spark = SparkSession \
+            .builder \
+            .appName('input_data_postgresql') \
+            .config("spark.jars", "./postgresql-42.2.18.jar") \
+            .config('spark.driver.extraClassPath', './postgresql-42.2.18.jar') \
+            .getOrCreate()
+    my_spark.sparkContext.setLogLevel('ERROR')
     
-    #create hadoop client
-    client = InsecureClient(credential['hadoop_data_lake'])
-    
-    #iteration over the tables_list
-    for table in tables_list :
-        start_time=time.time()
-        
-        #read postgresql data
+    for file in tables_list:
+        start_time = time.time()
+        #read csv as pandas dataframe
         schema = 'final_project'
-        data = pd.read_sql_table(table,con=engine,schema=schema)
-        datacount = data.shape[0]
+        data = my_spark.read.jdbc(url=url, table=f'{schema}.{file}', properties=properties)
+        datacount = data.count()
         
-        #write data into hadoop file system
-        hadoop_path = f'/FinalProject/{table}{time}.csv'
-        with client.write(hadoop_path, encoding='utf-8') as writer:
-            data.to_csv(writer)
-            
-        end_time=time.time()
-        print(f'[Data Ingestion] data {table} ({datacount} rows) has been stored in hdfs for {end_time-start_time} second')
+        #load data to hdfs        
+        date = datetime.now().strftime("%Y%m%d")
+        folder_name = f'FinalProject_{date}'
+        data.coalesce(1).write.mode("overwrite").option("header",True).csv(f"hdfs:///{folder_name}/{file}.csv")
+
+        end_time = time.time()
+        #log per file stored
+        print(f"[Data Ingestion] file '{file}.csv' ({datacount} rows) has been created for {round(end_time-start_time,2)} second")
+        
+        
+        
+        
         
 if __name__ == '__main__':
     print(f"[Data Ingestion] start")
+    print(f'[Data Ingestion] PostgreSQL to HDFS start')
     tables_list = ['distribution_centers','employees']
     postgresql_to_hdfs(tables_list)
+    print(f'[Data Ingestion] PostgreSQL to HDFS end')
+    print(f'[Data Ingestion] MongoDB to HDFS start')
+    tables_list = ['distribution_centers','employees']
+    print(f'[Data Ingestion] MongoDB to HDFS end')
+    
     
